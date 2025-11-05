394efb02b5bbce2114d965dd0ed675877c54de2f
eyuuab <eyobedabreham9@gmail.com>
2025-11-05 11:24:58 +0300

    add transparecy

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,7 +5,7 @@ return {
     priority = 1000,
     opts = {
       style = "night", -- "storm", "night", "day"
-      transparent = false,
+      transparent = true,
     },
     config = function(_, opts)
       require("tokyonight").setup(opts)
