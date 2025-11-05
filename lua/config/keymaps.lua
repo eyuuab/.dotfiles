d4b0768b54d341e6faebe17135e229f394caa18e
eyuuab <eyobedabreham9@gmail.com>
2025-11-05 11:25:45 +0300

    close buffer binding

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- Close the current buffer with ;bd
+vim.keymap.set("n", ";bd", "<cmd>bd<CR>", { desc = "Close current buffer" })
+-- Close all buffers with ;bdd
+vim.keymap.set("n", ";bdd", "<cmd>bufdo bd<CR>", { desc = "Close all buffers" })
